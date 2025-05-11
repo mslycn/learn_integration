755d3b4fd7c1ceaf66e87cd9da4c4c0d27300f25
mslycn <365775558@qq.com>
2025-05-12 04:00:12 +0800

    modify  file __init__.py

--- custom_components/learn_integration/__init__.py
+++ custom_components/learn_integration/__init__.py
@@ -1,6 +1,10 @@
-DOMAIN = "hello_state"
+DOMAIN = "learn_integration"
 
 async def async_setup(hass, config):
-    await hass.states.async_set("hello_state.world", "Paulus")
+    await hass.states.async_set("learn_integration.world", "hello word")
     # 返回布尔值以指示初始化成功
     return True
+
+# https://blog.csdn.net/tre4321/article/details/144350503
+
+
