6092b2b6307a9a88ec6b651837489e30101863a2
mslycn <365775558@qq.com>
2025-05-13 05:16:43 +0800

    创建一个实体， ID 为 learn_integration.world，状态值为 "hello word"

---
+++ README.md
@@ -0,0 +1,5 @@
+创建一个名为“learn-integration”的Home Assistant自定义集成
+
+首先，对于不熟悉Home Assistant集成的新手来说，可能不了解代码的结构。可能对各个部分的功能和它们如何协同工作不太清楚。
+
+代码包括目录结构、manifest.json配置、init.py的核心代码，以及测试步骤等。
--- custom_components/learn_integration/__init__.py
+++ custom_components/learn_integration/__init__.py
@@ -1,15 +1,15 @@
+# custom_components/learn_integration/__init__.py
+
 import asyncio
 
 
 DOMAIN = "learn_integration"
 
 async def async_setup(hass, config):
-    await hass.states.async_set("learn_integration.world", "hello word")
+    hass.states.async_set("learn_integration.world", "hello word")
+    # Return boolean to indicate that initialization was successful.
     # 返回布尔值以指示初始化成功
     return True
 
 # https://developers.home-assistant.io/docs/creating_component_index#the-minimum
 # https://blog.csdn.net/tre4321/article/details/144350503
-
-
-
